403420a57d0dc6a57ee6c05dc7ff5c7ededa0a5b
Baria Aziz <baria.aziz@metdesk.com>
2023-03-09 13:12:34 +0000

    Import Shoelace components, small style modifications

--- components/SLComponents.tsx
+++ components/SLComponents.tsx
@@ -9,3 +9,38 @@ export const SlIcon = dynamic(
   () => import("@shoelace-style/shoelace/dist/react/icon"),
   { ssr: false }
 );
+
+export const SlInput = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/input"),
+  { ssr: false }
+);
+
+export const SLSelect = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/select"),
+  { ssr: false }
+);
+
+export const SLOption = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/option"),
+  { ssr: false }
+);
+
+export const SLRange = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/range"),
+  { ssr: false }
+);
+
+export const SlDropdown = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/dropdown"),
+  { ssr: false }
+);
+
+export const SlMenu = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/menu"),
+  { ssr: false }
+);
+
+export const SlMenuItem = dynamic(
+  () => import("@shoelace-style/shoelace/dist/react/menu-item"),
+  { ssr: false }
+);
