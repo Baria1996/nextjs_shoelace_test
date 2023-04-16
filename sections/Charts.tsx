4f4f29356386a34a7f196c0ce80490cd388ed385
Baria Aziz <baria.aziz@metdesk.com>
2023-04-16 13:55:09 +0100

    Add windbarb series to test chart

--- sections/Charts.tsx
+++ sections/Charts.tsx
@@ -15,10 +15,46 @@ export default function Charts() {
   // setChartHeight(chartsWrapperHeight / numberOfCharts - 1);
   // }, []);
 
+  const seriesData = [12, 23, 22, 21, 13, 14, 15];
+
   const chartContainerStyle = {
     height: chartContainerHeight,
   };
   const chartConfig = { height: chartHeight };
+
+  const dims = {
+    time: 0,
+    windSpeed: 1,
+    R: 2,
+    waveHeight: 3,
+    weatherIcon: 2,
+    minTemp: 3,
+    maxTemp: 4,
+  };
+  const iconSize = 15;
+
+  const renderWindBarb = function (param: any, api: any) {
+    const point = api.coord([api.value(dims.time), api.value(dims.windSpeed)]);
+    console.log(point);
+    return {
+      type: "group",
+      children: [
+        {
+          type: "image",
+          style: {
+            image: "/assets/images/wind_barbs/wind_barbs_5.png",
+            x: -iconSize / 2,
+            y: -iconSize / 2,
+            width: iconSize,
+            height: iconSize,
+          },
+          // rotation: directionMap[api.value(dims.R)],
+          position: point,
+        },
+      ],
+    };
+  };
+
   const chartOptions1 = {
     xAxis: {
       type: "category",
@@ -29,7 +65,34 @@ export default function Charts() {
     },
     series: [
       {
-        data: [12, 23, 22, 21, 13, 14, 15],
+        data: seriesData,
+        type: "line",
+        clip: true,
+      },
+      {
+        type: "custom",
+        renderItem: renderWindBarb,
+        // encode: {
+        //   x: dims.time,
+        //   y: dims.windSpeed,
+        // },
+        data: [2, 13, 5, 2, 13, 24, 5],
+        z: 10,
+      },
+    ],
+  };
+
+  const chartOptions2 = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: seriesData,
         type: "line",
         clip: true,
       },
@@ -45,22 +108,22 @@ export default function Charts() {
       />
       <EChartsReact
         style={chartContainerStyle}
-        option={chartOptions1}
+        option={chartOptions2}
         opts={chartConfig}
       />
       <EChartsReact
         style={chartContainerStyle}
-        option={chartOptions1}
+        option={chartOptions2}
         opts={chartConfig}
       />
       <EChartsReact
         style={chartContainerStyle}
-        option={chartOptions1}
+        option={chartOptions2}
         opts={chartConfig}
       />
       <EChartsReact
         style={chartContainerStyle}
-        option={chartOptions1}
+        option={chartOptions2}
         opts={chartConfig}
       />
     </div>
